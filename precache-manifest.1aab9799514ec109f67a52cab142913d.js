3082aea7fede43c0f02b57e8773e5738b6f318c9
giladsolter <gilad.solter@gmail.com>
2018-10-23 16:35:44 +0300

    Updates

---
+++ precache-manifest.1aab9799514ec109f67a52cab142913d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crunch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1d1fb525a424bc31e72",
+    "url": "/crunch/static/js/runtime~main.b1d1fb52.js"
+  },
+  {
+    "revision": "92fdd24053de50429102",
+    "url": "/crunch/static/js/main.92fdd240.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/crunch/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "92fdd24053de50429102",
+    "url": "/crunch/static/css/main.cbef9161.chunk.css"
+  },
+  {
+    "revision": "0a679cdc856c05175962770a6085ab1a",
+    "url": "/crunch/index.html"
+  }
+];
